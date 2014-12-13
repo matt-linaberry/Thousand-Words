c38b2d88bb00bf26e06cb3cb5fa6182c94a638f4
Matt Linaberry <matt.linaberry@we-ics.com>
2014-12-13 16:37:07 -0500

    Initial push -
    Thru Core Data an Initial Light Dusting.

---
+++ Thousand Words/TWAlbumTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TWAlbumTableViewController.h
+//  Thousand Words
+//
+//  Created by Matthew Linaberry on 12/13/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAlbumTableViewController : UITableViewController
+@property (strong, nonatomic) NSMutableArray *albums;
+@end

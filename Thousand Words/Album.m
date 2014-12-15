428bb0b14435ef9535a1ba39fe31aa49484b1217
Matt Linaberry <matt.linaberry@we-ics.com>
2014-12-14 21:49:38 -0500

    intro to core data

---
+++ Thousand Words/Album.m
@@ -0,0 +1,17 @@
+//
+//  Album.m
+//  Thousand Words
+//
+//  Created by Matthew Linaberry on 12/14/14.
+//  Copyright (c) 2014 Matthew Linaberry. All rights reserved.
+//
+
+#import "Album.h"
+
+
+@implementation Album
+
+@dynamic name;
+@dynamic date;
+
+@end
